434301c7a83fe432d30abbff0b84c947d55bffca
Sharlin Tang <sharlin_tang@dlsu.edu.ph>
2021-08-20 18:42:22 +0800

    Added needed Activities, edited register layout

---
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.manuel.tang.strayhaven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
