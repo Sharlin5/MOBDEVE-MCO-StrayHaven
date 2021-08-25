deb0f26bf3d59d715854f2d987118edcd6e5baad
Sharlin Tang <sharlin_tang@dlsu.edu.ph>
2021-08-25 15:13:51 +0800

    added initComponents

--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/ProfileActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/ProfileActivity.java
@@ -11,4 +11,8 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void initComponents(){
+
+    }
 }
