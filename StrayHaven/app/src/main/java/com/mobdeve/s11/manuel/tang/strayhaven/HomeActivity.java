153988eb50e09c2698f7df75956a3727f1e3a68d
Sharlin Tang <sharlin_tang@dlsu.edu.ph>
2021-08-25 16:42:43 +0800

    Added login functionality

--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/HomeActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/HomeActivity.java
@@ -2,7 +2,9 @@ package com.mobdeve.s11.manuel.tang.strayhaven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.initComponents();
     }
 
     //Initialize objects
@@ -26,6 +29,14 @@ public class HomeActivity extends AppCompatActivity {
         this.ibTracker = findViewById(R.id.ib_tracker);
         this.ibSettings = findViewById(R.id.ib_settings);
         this.fabPost = findViewById(R.id.fab_create_post);
+
+        fabPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MainActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MainActivity.java
@@ -2,17 +2,34 @@ package com.mobdeve.s11.manuel.tang.strayhaven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initComponents();
     }
 
     public void initComponents(){
+        btnLogin = findViewById(R.id.btn_login_confirm);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
