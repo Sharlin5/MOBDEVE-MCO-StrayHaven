50402fbd337ad662d48a33e79b007df903f0bb0e
Sharlin Tang <sharlin_tang@dlsu.edu.ph>
2021-08-25 20:58:27 +0800

    Added Login Functionality

--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MainActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobdeve.s11.manuel.tang.strayhaven;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,30 +8,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etEmail, etPassword;
     private Button btnLogin;
     private TextView btnRegister;
+    private ProgressBar pbLogin;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.initComponents();
+        this.initFirebase();
     }
 
     public void initComponents(){
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        pbLogin = findViewById(R.id.pb_login);
+
         btnLogin = findViewById(R.id.btn_login_confirm);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                signIn(email, password);
             }
         });
 
@@ -43,5 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initFirebase(){
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void signIn(String email, String password){
+        pbLogin.setVisibility(View.VISIBLE);
+        this.mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    private boolean checkValid(String email, String password){
+        boolean isValid = true;
+        if(email.isEmpty()){
+            this.etEmail.setError("Required Field");
+            isValid = false;
+        }
+        if (password.isEmpty()){
+            this.etPassword.setError("Required Field");
+            isValid = false;
+        }
+        return isValid;
     }
 }
