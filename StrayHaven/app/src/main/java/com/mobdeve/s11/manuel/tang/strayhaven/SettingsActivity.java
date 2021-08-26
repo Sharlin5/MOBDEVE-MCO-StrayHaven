79b5742f122349eba403924d080d8f62fa769caa
Sharlin Tang <sharlin_tang@dlsu.edu.ph>
2021-08-26 21:22:21 +0800

    Update SettingsActivity.java

--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/SettingsActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/SettingsActivity.java
@@ -1,23 +1,40 @@
 package com.mobdeve.s11.manuel.tang.strayhaven;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private ImageButton ibBack, ibHome, ibTracker, ibNotifications, ibMessages;
     private Button btnLogout;
+    private EditText etName, etDescription, etLocation;
+
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         this.initComponents();
+        this.initFirebase();
         overridePendingTransition(0,0);
         getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
     }
@@ -36,6 +53,9 @@ public class SettingsActivity extends AppCompatActivity {
         this.ibNotifications = findViewById(R.id.ib_settings_notifications);
         this.ibMessages = findViewById(R.id.ib_settings_messages);
         this.btnLogout = findViewById(R.id.btn_settings_logout);
+        this.etName = findViewById(R.id.et_settings_name);
+        this.etDescription = findViewById(R.id.et_settings_desc);
+        this.etLocation =findViewById(R.id.et_settings_loc);
 
         ibBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +102,27 @@ public class SettingsActivity extends AppCompatActivity {
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+    }
+
+    private void initFirebase(){
+        this.mAuth = FirebaseAuth.getInstance();
+        this.user = this.mAuth.getCurrentUser();
+        this.userId = this.user.getUid();
+        this.database = FirebaseDatabase.getInstance();
+
+        DatabaseReference reference = database.getReference(Collections.users.name());
+        reference.child(this.userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("profilename").getValue().toString();
+                etName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
