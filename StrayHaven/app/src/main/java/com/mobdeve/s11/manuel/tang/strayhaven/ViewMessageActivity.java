6b9b701ff1f3b1949ac9bc4b4f18b6e22e4f7c85
Aika Manuel <aika_manuel@dlsu.edu.ph>
2021-08-27 03:08:42 +0800

    Overall v0.5 (Post and messages not finished yet)

---
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/ViewMessageActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.manuel.tang.strayhaven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_message);
+    }
+}
