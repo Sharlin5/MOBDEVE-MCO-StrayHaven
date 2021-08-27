358c2116240c6277932d90b6004f8df9af6c085b
Aika Manuel <aika_manuel@dlsu.edu.ph>
2021-08-27 17:01:54 +0800

    Message Activity and Updated Tracker Template v0.1

--- StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MessagesActivity.java
+++ StrayHaven/app/src/main/java/com/mobdeve/s11/manuel/tang/strayhaven/MessagesActivity.java
@@ -2,13 +2,82 @@ package com.mobdeve.s11.manuel.tang.strayhaven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MessagesActivity extends AppCompatActivity {
+
+    private ImageView ivProfile;
+    private ImageButton ibSettings, ibHome, ibTracker, ibNotifications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+        this.initComponents();
+        overridePendingTransition(0,0);
+        getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
+    //Initialize objects
+    public void initComponents(){
+        this.ivProfile = findViewById(R.id.iv_message_user_pic);
+        this.ibSettings = findViewById(R.id.ib_message_settings);
+        this.ibHome = findViewById(R.id.ib_message_home);
+        this.ibTracker = findViewById(R.id.ib_message_tracker);
+        this.ibNotifications = findViewById(R.id.ib_message_notifications);
+
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ibSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ibHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, HomeRequestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ibTracker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, TrackerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ibNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessagesActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
